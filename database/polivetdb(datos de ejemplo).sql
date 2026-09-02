832e13103798abdffa84387c9626b63ec14f47ad
Bryan Simoes <bryansimoes298@gmail.com>
2026-09-01 21:29:16 -0300

    Correcciones en la Estructura de la Base de Datos y se Agregó otro Archivo que También incluye Datos de Ejemplo
    Se corrigió el límite de caracteres en la columna "precio_servicio" de la tabla "servicio".
    
    Se creó otro archivo de importación de la base de datos que incluye datos de ejemplo (solo en las tablas "paciente", "tutor", "usuarios" y "servicio"). Dichos archivos  ahora incluyen la sentencia de creación de la base de datos.

--- database/polivetdb(datos de ejemplo).sql
+++ database/polivetdb(datos de ejemplo).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 01-09-2026 a las 23:14:44
+-- Tiempo de generación: 02-09-2026 a las 00:26:16
 -- Versión del servidor: 10.11.18-MariaDB-0+deb12u1
 -- Versión de PHP: 8.2.32
 
@@ -77,6 +77,15 @@ CREATE TABLE `paciente` (
   `dateupdate_paciente` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `paciente`
+--
+
+INSERT INTO `paciente` (`id_paciente`, `id_tutor`, `nombre_paciente`, `color_paciente`, `foto_paciente`, `fecha_nac_paciente`, `sexo_paciente`, `peso_paciente`, `esterilizado_paciente`, `datecreate_paciente`, `dateupdate_paciente`) VALUES
+(1, 2, 'Bondiola', 'Marrón', 'wadasw', '2026-08-17', 'Macho', 100.50, 'No', '2026-09-01 20:54:37', '2026-09-01 20:54:37'),
+(2, 1, 'Juancho', 'Blanco', 'awsaw', '2025-07-02', 'Macho', 7.50, 'Si', '2026-09-01 23:54:37', '2026-09-01 23:54:37'),
+(3, 1, 'Trapecia', 'Negro', 'wadsdw', '2023-09-15', 'Hembra', 10.70, 'Si', '2026-09-01 21:01:58', '2026-09-01 21:01:58');
+
 -- --------------------------------------------------------
 
 --
@@ -94,6 +103,13 @@ CREATE TABLE `servicio` (
   `datecreate_servicio` datetime NOT NULL,
   `dateupdate_servicio` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `servicio`
+--
+
+INSERT INTO `servicio` (`id_servicio`, `id_usuario`, `nombre_servicio`, `descripcion_servicio`, `precio_servicio`, `imagenURL_servicio`, `iconoURL_servicio`, `datecreate_servicio`, `dateupdate_servicio`) VALUES
+(1, 1, 'Consulta', 'Se realiza una consulta médica al animal', 999.99, 'awsdasaws', 'wasdwa', '2026-09-02 21:21:58', '2026-09-02 21:21:58');
 
 -- --------------------------------------------------------
 
@@ -114,6 +130,14 @@ CREATE TABLE `tutor` (
   `dateupdate_tutor` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `tutor`
+--
+
+INSERT INTO `tutor` (`id_tutor`, `documento_tutor`, `nombre_tutor`, `apellido_tutor`, `telefono_tutor`, `dirección_tutor`, `email_tutor`, `notas_tutor`, `datecreate_tutor`, `dateupdate_tutor`) VALUES
+(1, 73268451, 'Martina', 'Barboza', '+59891234567', 'En su casa', 'martina@gmail.com', NULL, '2026-09-01 20:46:33', '2026-09-01 20:46:33'),
+(2, 10007818, 'Andrés', 'Ferreira', '+59812345678', 'En otra casa', 'andrés@email.com', NULL, '2026-09-01 21:46:33', '2026-09-01 21:46:33');
+
 -- --------------------------------------------------------
 
 --
@@ -131,6 +155,14 @@ CREATE TABLE `usuarios` (
   `lastlogin_usuario` datetime NOT NULL,
   `dateupdate_usuario` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `cedula_usuario`, `nombre_usuario`, `apellido_usuario`, `rol_usuario`, `email_usuario`, `passwd_usuario`, `lastlogin_usuario`, `dateupdate_usuario`) VALUES
+(1, 23885203, 'Hector', 'Dir', 'director', 'dirhector@polivet.com', '$2y$10$7oZTaVgRnCUWre82w1JE0usixwMRZhuXBwJB.LdnvK78JH4S581H.', '2026-09-01 20:17:55', '2026-09-01 20:17:55'),
+(2, 97454155, 'Roberta', 'Hernandez', 'Estudiante', 'robertah@polivet.com', '$2y$10$L3pa1ANJtX0Bzf6pm2PXWOdmAfDNwrSqiXSnxeT/lBhBdx8aTlNbi', '2026-09-01 23:17:55', '2026-09-01 23:17:55');
 
 -- --------------------------------------------------------
 
@@ -173,7 +205,8 @@ ALTER TABLE `paciente`
 -- Indices de la tabla `servicio`
 --
 ALTER TABLE `servicio`
-  ADD PRIMARY KEY (`id_servicio`);
+  ADD PRIMARY KEY (`id_servicio`),
+  ADD KEY `servicio_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `tutor`
@@ -215,25 +248,25 @@ ALTER TABLE `consulta`
 -- AUTO_INCREMENT de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `id_paciente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_paciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `servicio`
 --
 ALTER TABLE `servicio`
-  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `tutor`
 --
 ALTER TABLE `tutor`
-  MODIFY `id_tutor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -257,6 +290,12 @@ ALTER TABLE `consulta`
 --
 ALTER TABLE `paciente`
   ADD CONSTRAINT `fk_paciente_tutor` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id_tutor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD CONSTRAINT `servicio_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
 
 --
 -- Filtros para la tabla `usuario_consulta`
